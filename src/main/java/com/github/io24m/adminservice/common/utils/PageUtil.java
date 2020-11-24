cf57d22d1950dce7cf8230b5a21e8233df4e83ec
io24m <io24m@outlook.com>
2020-11-24 15:05:28 +0800

    i

--- src/main/java/com/github/io24m/adminservice/common/utils/PageUtil.java
+++ src/main/java/com/github/io24m/adminservice/common/utils/PageUtil.java
@@ -1,5 +1,6 @@
 package com.github.io24m.adminservice.common.utils;
 
+import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
 
 import javax.servlet.http.HttpServletRequest;
@@ -10,10 +11,10 @@ import javax.servlet.http.HttpServletRequest;
  * @create 2020-11-24 14:54
  */
 public class PageUtil {
-    public static void startPage() {
+    public static <E> Page<E> startPage() {
         HttpServletRequest request = RequestUtil.getRequest();
         int pageNum = Integer.valueOf(request.getParameter("pageNum"));
         int pageSize = Integer.valueOf(request.getParameter("pageSize"));
-        PageHelper.startPage(pageNum, pageSize);
+        return PageHelper.startPage(pageNum, pageSize);
     }
 }
