aac8c9978b78e18d89ad9d15878e998e4b900548
io24m <io24m@outlook.com>
2020-11-24 17:01:29 +0800

    i

--- src/main/java/com/github/io24m/adminservice/AdminServiceApplication.java
+++ src/main/java/com/github/io24m/adminservice/AdminServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.github.io24m.adminservice")
+@MapperScan("com.github.io24m.adminservice.mapper")
 public class AdminServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(AdminServiceApplication.class, args);
