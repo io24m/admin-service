84d8e90ee7dbcc70dbd69e4aa127c20f3c9f2693
io24m <io24m@outlook.com>
2021-01-21 16:28:32 +0800

    i

--- src/main/java/com/github/io24m/adminservice/common/utils/TokenUtil.java
+++ src/main/java/com/github/io24m/adminservice/common/utils/TokenUtil.java
@@ -3,6 +3,8 @@ package com.github.io24m.adminservice.common.utils;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 
+import java.util.Date;
+
 /**
  * @author lk1
  * @description
@@ -14,8 +16,14 @@ public final class TokenUtil {
      * 根据用户生成token
      */
     public static String getToken(String account, String password) {
+        Date date = new Date();
         String token = JWT.create()
                 .withAudience(account)
+                .withIssuedAt(date)
+                .withNotBefore(date)
+//                .withExpiresAt()
+//                .withClaim("iat", date)
+//                .withClaim("nbf", date)
                 .sign(Algorithm.HMAC256(password));
         return token;
     }
