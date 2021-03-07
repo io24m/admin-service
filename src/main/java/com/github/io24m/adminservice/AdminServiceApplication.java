be8bdc5ccd1e2f3e937056af5503ff4ed65b2390
io24m <io24m@outlook.com>
2021-03-07 19:52:33 +0800

    i

--- src/main/java/com/github/io24m/adminservice/AdminServiceApplication.java
+++ src/main/java/com/github/io24m/adminservice/AdminServiceApplication.java
@@ -3,11 +3,20 @@ package com.github.io24m.adminservice;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.context.request.RequestContextListener;
 
 @SpringBootApplication
 //@MapperScan("com.github.io24m.adminservice")
 @MapperScan
+@EnableOAuth2Sso
 public class AdminServiceApplication {
+    @Bean
+    public RequestContextListener requestContextListener() {
+        return new RequestContextListener();
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(AdminServiceApplication.class, args);
     }
