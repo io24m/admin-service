6f8dc4f963059ff15e0aba15d9ec99596a0e8723
io24m <io24m@outlook.com>
2021-03-08 10:40:43 +0800

    i

--- src/main/java/com/github/io24m/adminservice/config/WebSecurityConfig.java
+++ src/main/java/com/github/io24m/adminservice/config/WebSecurityConfig.java
@@ -1,38 +1,38 @@
-package com.github.io24m.adminservice.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.authentication.logout.LogoutFilter;
-
-@Order(1)
-@Configuration
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-
-        http.authorizeRequests()
-                .antMatchers("/", "/index").authenticated()
-                //region 权限码
-//                .requestMatchers(getRequestMatcher("/sys/user/**")).hasAuthority("sys-user")
-//                .requestMatchers(getRequestMatcher("/sys/notication/**")).hasAuthority("sys-notication")
-//                .requestMatchers(getRequestMatcher("/sys/hospitalgp/**")).hasAuthority("sys-hospitalgp")
-//                //endregion
-                .antMatchers("/**/**").authenticated()
-                .anyRequest().hasAuthority("not_allow")//这一行强制添加，必须加
-                .and().logout().permitAll()
-                //关闭跨站请求防护
-                .and().csrf().disable()
-                .headers().frameOptions().sameOrigin();
-
+//package com.github.io24m.adminservice.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.core.annotation.Order;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.web.authentication.logout.LogoutFilter;
+//
+////@Order(1)
+//@Configuration
+//public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//
 //        http.authorizeRequests()
 //                .antMatchers("/", "/index").authenticated()
-//                .antMatchers("/login", "/logout").permitAll()
-//                .antMatchers("/**").authenticated()
-////                .anyRequest().hasAuthority("not_allow")
+//                //region 权限码
+////                .requestMatchers(getRequestMatcher("/sys/user/**")).hasAuthority("sys-user")
+////                .requestMatchers(getRequestMatcher("/sys/notication/**")).hasAuthority("sys-notication")
+////                .requestMatchers(getRequestMatcher("/sys/hospitalgp/**")).hasAuthority("sys-hospitalgp")
+////                //endregion
+//                .antMatchers("/**/**").authenticated()
+//                .anyRequest().hasAuthority("not_allow")//这一行强制添加，必须加
 //                .and().logout().permitAll()
-//                .and().csrf().disable();
-    }
-}
+//                //关闭跨站请求防护
+//                .and().csrf().disable()
+//                .headers().frameOptions().sameOrigin();
+//
+////        http.authorizeRequests()
+////                .antMatchers("/", "/index").authenticated()
+////                .antMatchers("/login", "/logout").permitAll()
+////                .antMatchers("/**").authenticated()
+//////                .anyRequest().hasAuthority("not_allow")
+////                .and().logout().permitAll()
+////                .and().csrf().disable();
+//    }
+//}
