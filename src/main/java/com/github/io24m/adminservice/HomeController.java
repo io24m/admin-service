772e59feeb1eb3a5b22fe7e48fbcaa4103032116
io24m <io24m@outlook.com>
2021-03-12 10:24:38 +0800

    排除swagger接口[/]

--- src/main/java/com/github/io24m/adminservice/HomeController.java
+++ src/main/java/com/github/io24m/adminservice/HomeController.java
@@ -1,29 +1,31 @@
 package com.github.io24m.adminservice;
 
 import com.github.io24m.adminservice.common.annotation.SkipToken;
-import com.github.io24m.adminservice.common.dto.AjaxResponse;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * @author lk1
  * @description
  * @create 2021-01-21 12:36
  */
-
+@ApiIgnore
 @Api(description = "首页")
-@RestController
+@Controller
 @RequestMapping("")
 public class HomeController {
 
     @ApiOperation("首页")
     @SkipToken
     @GetMapping("")
-    public AjaxResponse index() {
-        String res = "文档地址[/doc.html|/swagger-ui.html]";
-        return AjaxResponse.result(res);
+    public void index(HttpServletResponse response) throws IOException {
+        response.sendRedirect("doc.html");
     }
 }
